803a88153e6c8e19495e5a2d8d51cdbd0ad198e2
farouq <48164417+siwoku@users.noreply.github.com>
2020-01-15 17:25:55 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+ let game   ;
+ const resetDisplay = () => {
+     const overlay = document.querySelector("#overlay");
+     overlay.style.display ="none";
+     overlay.className = "start"
+ }
+
+ const markButton = letter => {												//marks the received key
+	letter.disabled = true;
+	letter.className += ` chosen`;
+}
+
+ 
+
+ const keyEvent = e => {
+     if(/^[a-zA-Z]$/.test(e.key)){
+         document.querySelectorAll('.key').forEach(button => {
+             
+             if(button.textContent == e.key && !button.disabled){
+                 game.handleInteraction(e.key);
+                 markButton(button)
+                 return;
+             }
+         })
+     }
+ }
+
+ document.querySelector(`#btn__reset`).addEventListener(`click`, () => {		//when the user presses the start or try again button, a new game is created,
+	resetDisplay();															//the overlay is hidden and starts the game
+	game = new Game();
+	game.startGame();
+});
+document.querySelector(`#qwerty`).addEventListener(`click`, e => {			//on screen key event listener, marks the key on screen and 
+	if (e.target.tagName === `BUTTON`){										//handles the interaction
+		markButton(e.target);
+		game.handleInteraction(e.target.textContent);
+	}
+});
